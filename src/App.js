f34bc8de5d65a27cd41c20db340c772ca1d2143a
Delia <delia_cardenas_30@hotmail.com>
2023-05-31 20:07:01 -0300

    probando

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Inicio from "./components/paginas/Inicio";
+import Busqueda from "./components/paginas/Busqueda";
+import SobreNosotras from "./components/paginas/SobreNosotras";
 import Formulario from "./components/Formulario";
 
 
 function App() {
   return (
-   <div className="app">
-    <h1>Weather app</h1>
-		<Formulario />
-   </div>
-  
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path= '/' element = {<Inicio></Inicio>}/>
+          <Route path= '/Busqueda.js' element = {<Busqueda/>}/>
+          <Route path= '/SobreNosotras.js' element = {<SobreNosotras/>}/>
+          <Route path= '/Contacto.js' element = {<Formulario/>}/>
+        </Routes>
+        <Navbar/>
+        <Formulario/>
+      </Router>
+      <h1>Weather app</h1>
+      
+    </div>
   );
 }
 
