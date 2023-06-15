815913e1236288f970543bb7f1765e1eff0334d9
Maria Uzcategui <125880293+mariaU1985@users.noreply.github.com>
2023-06-15 15:27:26 -0300

    cambio de fondo e icono

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import videoLluvia from '../assets/video-fondo-lluvia.mp4';
-import videoNieve from '../assets/video-fondo-nieve.mp4';
-import videoSol from '../assets/video-fondo-sol.mp4';
-import '../css/landingpage.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/landingpage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMousePointer } from "@fortawesome/free-solid-svg-icons";
+import videoLluvia from "../assets/video-fondo-lluvia.mp4";
+import img1 from "../assets/img1.png";
+import img3 from "../assets/img3.png";
 
 const Landing = () => {
   return (
     <div className="containerlanding">
       <div className="video-container">
-        <video className="video-background1" autoPlay muted loop>
-          <source src={videoSol} type="video/mp4" />
-        </video>
         <video className="video-background2" autoPlay muted loop>
           <source src={videoLluvia} type="video/mp4" />
         </video>
-        <video className="video-background3" autoPlay muted loop>
-          <source src={videoNieve} type="video/mp4" />
-        </video>
+        <img src={img1} alt="Imagen1" />
+        <img src={img3} alt="Imagen3" />
       </div>
-      <Link to="/inicio" className="image-caption">Que no te tome por sorpresa</Link>
+      <Link to="/inicio" className="image-caption">
+        <span className="caption-text">Que no te tome por sorpresa</span>
+        <FontAwesomeIcon icon={faMousePointer} className="caption-icon" />
+      </Link>
     </div>
   );
 };
